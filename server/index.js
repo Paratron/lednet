689edac6df0399b666da1ac8bdbf818787a4bc14
Christian Engel <chrispy@parastudios.de>
2020-10-18 00:00:16 +0200

    Add some log messages to the server

--- server/index.js
+++ server/index.js
@@ -4,7 +4,11 @@ const led = require("./led");
 net.start(net.SERVER);
 
 net.on('cmd', (data) => {
+    console.log("Received command", data);
+
     const {method, ...arguments} = data;
     led[method].call(arguments);
 });
+
+console.log("LED net started");
 
