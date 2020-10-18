15a24b7ba46d5dcf8a1b350ea85b826c0bfb5a57
Christian Engel <chrispy@parastudios.de>
2020-10-18 02:37:53 +0200

    try spec fix

--- client/index.js
+++ client/index.js
@@ -2,7 +2,7 @@ const net = require("../shared/net");
 const led = require("./led");
 
 net.logMessages = true;
-net.start(net.CLIENT, led.getConfig);
+net.start(net.CLIENT, () => led.getConfig());
 
 net.on('cmd', (data) => {
     console.log("Received command", data);
