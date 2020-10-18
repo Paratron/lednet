b69991530e8cb5c70f4bf18a40e4fb1f66d4b29e
Christian Engel <chrispy@parastudios.de>
2020-10-18 02:14:11 +0200

    Small improvement on server

--- sdk/index.js
+++ sdk/index.js
@@ -1,9 +1,18 @@
 const net = require("../shared/net");
 
+net.logMessages = true;
 net.start(net.SERVER);
+
+let target;
+
+function send(message, data, meta){
+    net.send("cmd", {message: message, arguments: data, meta: Object.assign({}, {instanceId: target})});
+}
 
 module.exports = {
     discoverClients: net.discoverClients,
+    useClient: ({instanceId}) => target = instanceId,
 
-    setColor: (r, g, b) => net.send("setColor", {r,g,b})
+    init: (options) => send("init", options, ),
+    setColor: (r, g, b) => send("setColor", {r,g,b}, {instanceId: target})
 };
