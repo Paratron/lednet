b57defff6dc5d9ce00888cbc41dcc28402849ec9
Christian Engel <chrispy@parastudios.de>
2020-10-18 02:04:01 +0200

    net update

--- shared/net.js
+++ shared/net.js
@@ -77,10 +77,6 @@ module.exports = {
      * @param {string} [target] IP address. Omit to broadcast.
      */
     send: (type, data, meta) => {
-        if (netMode === MODES.CLIENT && !target) {
-            target = serverAddress;
-        }
-
         if (module.exports.logMessages) {
             console.log(">>S>>", {type, data});
         }
