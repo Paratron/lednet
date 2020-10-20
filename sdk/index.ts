54de5490c93ba0cf6356b9b459acbc38b84dff51
Christian Engel <chrispy@parastudios.de>
2020-10-20 21:46:52 +0200

    Add SDK function to set individual pixel colors

--- sdk/index.ts
+++ sdk/index.ts
@@ -10,6 +10,7 @@ function remoteFunctionCall(method: string, data: any, clientId: string){
 interface ClientInterface extends Client {
     configure: (options: ConnectorConfig) => void;
     setColor: (r: number, g: number, b: number) => void;
+    setColorForPixel: (index: number, r: number, g: number, b: number) => void;
     tweenToColor: (r: number, g: number, b: number) => void;
 }
 
@@ -17,6 +18,7 @@ const clientInterface = (client: Client): ClientInterface => ({
     ...client,
     configure: (options: ConnectorConfig) => remoteFunctionCall("configure", [options], client.clientId ),
     setColor: (r: number, g: number, b: number) => remoteFunctionCall("setColor", [r,g,b], client.clientId),
+    setColorForPixel: (index: number, r: number, g: number, b: number) => remoteFunctionCall("setColorForPixel", [index,r,g,b], client.clientId),
     tweenToColor: (r: number, g: number, b: number) => remoteFunctionCall("tweenToColor", [r,g,b], client.clientId)
 });
 
