6d9d8aeff23fc088ead310a499d172a230d39f35
Christian Engel <chrispy@parastudios.de>
2020-10-23 00:59:18 +0200

    Add possibility to respond to messages into net stack

--- shared/net.ts
+++ shared/net.ts
@@ -1,4 +1,5 @@
 import { Socket } from "dgram"
+import { v4 as uuid } from 'uuid';
 
 export interface RemoteAddressInformation {
     address: string;
@@ -21,13 +22,19 @@ const BROADCAST_ADDR = '230.185.192.178';
 
 let clientId = Math.floor(Math.random() * 7634872394723).toString(32);
 
-export type EventListenerFunction = (props: {data: any, meta: any, rinfo: RemoteAddressInformation}) => void;
+export type EventListenerFunction = (props: {data: any, meta: any, rinfo: RemoteAddressInformation, response: (data: any) => void}) => void;
 
 interface EventListeners {
     [key: string]: EventListenerFunction[];
 }
 
 let eventListeners: EventListeners = {};
+let responseWaiters: {[key: string]: (data: any) => void} = {};
+
+interface Meta {
+    clientId?: string;
+    msgId: string;
+}
 
 export interface Client extends RemoteAddressInformation {
     spec: any;
@@ -40,12 +47,14 @@ let discoveryCollector: undefined | Client[];
 /**
  * Trigger an event on all listeners.
  */
-const trigger = (event: string, data: any, meta: any, rinfo: RemoteAddressInformation) => {
+const trigger = (event: string, data: any, meta: Meta, rinfo: RemoteAddressInformation) => {
     if (!eventListeners[event]) {
         return;
     }
 
-    eventListeners[event].forEach(cb => cb({data, meta, rinfo}));
+    const response = (responseData: any) => module.exports.send(event, responseData, {msgIdResponse: meta.msgId});
+
+    eventListeners[event].forEach(cb => cb({data, meta, rinfo, response}));
 };
 
 socket.on('message', (msg: Buffer, rinfo: RemoteAddressInformation) => {
@@ -74,6 +83,13 @@ socket.on('message', (msg: Buffer, rinfo: RemoteAddressInformation) => {
         return;
     }
 
+    if(json.meta.msgIdResponse){
+        if(responseWaiters[json.meta.msgIdResponse]){
+            responseWaiters[json.meta.msgIdResponse](json.data);
+        }
+        return;
+    }
+
     trigger(json.type, json.data, json.meta, rinfo);
 });
 
@@ -91,16 +107,22 @@ module.exports = {
      * Will create an UDP message and send it to the network.
      * Either specify a target, or omit to send a broadcast to all devices.
      */
-    send: (type: string, data: any, meta: any) => {
+    send: (type: string, data: any, meta: any, callback?: (data: any) => void) => {
         if (module.exports.logMessages) {
-            console.log(">>S>>", {type, data});
+            console.log(">>S>>", { type, data });
+        }
+
+        const msgId = uuid();
+
+        if(callback){
+            responseWaiters[msgId] = callback;
         }
 
         socket.send(
             JSON.stringify({
                 type,
                 data,
-                meta
+                meta: Object.assign({}, meta, {msgId})
             }),
             remotePort,
             BROADCAST_ADDR
